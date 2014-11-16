e2035f043a49aaff5d85c5c1b21fb0cf94a20068
goliatone <burgosemi@gmail.com>
2014-11-16 10:39:18 -0500

    Example: Added sockets

---
+++ examples/app/sockets.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function(anden){
+	console.log('RUNNING SOCKETS');
+	anden.sockets.on('connection', function(socket){
+		console.log('ON CONNECTION')
+		socket.on('message', function(data){
+			console.log('MESSAGE', data);
+		});
+	});
+};
