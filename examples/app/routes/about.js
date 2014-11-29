ba7d734e964feac0d6e72263f3a550895bd4cca8
goliatone <burgosemi@gmail.com>
2014-11-28 21:41:11 -0500

    example: update about

--- examples/app/routes/about.js
+++ examples/app/routes/about.js
@@ -1,27 +1,12 @@
 var Route = {};
 
 Route.priority = 10;
-Route.using = '/user';
 
 Route.register = function(app, options){
     console.log('- users: register routes');
-    app.get('/peperone', function(req, res){
-        res.send('Peperone is here, mofos!');
+    app.get('/about', function(req, res){
+        res.send('<h1>About</h1><p>This is an about page</p>');
     });
-};
-
-Route.routes = {
-    'GET /users': function(req, res) {
-        res.send('==> GET users');
-    },
-    'GET /:resource/:id': function(req, res) {
-        console.log('params', req.params);
-        res.send('===> GET user: '+ req.params.resource+ ' '+ req.params.id);
-    },
-    'POST /:resource': function(req, res) {},
-    'PUT /:resource/:id': function(req, res) {},
-    'PATCH /:resource/:id': function(req, res) {},
-    'DELETE /:resource/:id': function(req, res) {}
 };
 
 module.exports = Route;
