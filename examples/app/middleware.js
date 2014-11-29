44c04c9f410ea1393705fa1d3331904a236aec6b
goliatone <burgosemi@gmail.com>
2014-11-29 14:43:32 -0500

    EXAMPLE: wip app middleware

--- examples/app/middleware.js
+++ examples/app/middleware.js
@@ -5,6 +5,8 @@ var debug = require('debug')('app:middleware');
 module.exports = function(anden){
 	debug('MIDDLEWARE APP %s running', __filename);
 
+	return;
+
 	var staticPath = path.resolve('public');
 	debug('[%s] use express static %s %s', anden.name, staticPath);
     anden.app.use(anden.app.express.static(staticPath));
