375ce353e1812acb8cf39881fea93b0d76410eb6
goliatone <burgosemi@gmail.com>
2014-11-29 14:42:30 -0500

    EXAMPLE: admin app middleware WIP

--- examples/app/admin/middleware.js
+++ examples/app/admin/middleware.js
@@ -4,6 +4,8 @@ var path = require('path');
 module.exports = function(anden){
 	console.log('/////// ADMIN MIDDLEWARE',__filename,'running');
 
+	return;
+
 	var staticPath = path.join(__dirname, 'public');
 	console.log('STATIC PATH', staticPath)
     anden.app.use(anden.app.express.static(staticPath));
