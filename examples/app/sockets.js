5f4adb4733c906fb93a3f5be585c90c8a31d762a
goliatone <burgosemi@gmail.com>
2014-11-29 14:41:40 -0500

    EXAMPLE: app sockets wip

--- examples/app/sockets.js
+++ examples/app/sockets.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports = function(anden){
+module.exports = function(){
 	console.log('RUNNING SOCKETS');
-	anden.sockets.on('connection', function(socket){
+	/*anden.sockets.on('connection', function(socket){
 		console.log('ON CONNECTION')
 		socket.on('message', function(data){
 			console.log('MESSAGE', data);
 			socket.send(data)
 		});
-	});
+	});*/
 };
