bb82560e96598fe3110cc0d6e4d708adc284508c
goliatone <burgosemi@gmail.com>
2014-11-29 23:38:12 -0500

    EXAMPLE: Updated middleware app

--- examples/app/middleware.js
+++ examples/app/middleware.js
@@ -1,13 +1,24 @@
 'use strict';
-var path = require('path');
-var debug = require('debug')('app:middleware');
+var path = require('path'),
+	helper = require('../utils/moduleloader'),
+	cors = require('cors'),
+    logger = require('morgan'),
+    express = require('express'),
+    favicon = require('serve-favicon'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+	debug = require('debug')('app:middleware');
 
 module.exports = function(anden){
-	debug('MIDDLEWARE APP %s running', __filename);
+	debug('MIDDLEWARE APP %s running', this.name);
+	var staticPath = path.resolve('../../' + this.staticDir),
+    	faviconPath = path.join(staticPath, 'favicon.ico');
 
-	return;
-
-	var staticPath = path.resolve('public');
-	debug('[%s] use express static %s %s', anden.name, staticPath);
-    anden.app.use(anden.app.express.static(staticPath));
+    anden.app.use(favicon(faviconPath));
+    anden.app.use(logger('combined'));
+	anden.app.use(cors());
+	anden.app.use(bodyParser.json({ type: 'application/*+json' }));
+	anden.app.use(bodyParser.text({ type: 'text/html' }));
+	anden.app.use(bodyParser.urlencoded({ extended: false }));
+	anden.app.use(cookieParser());
 };
