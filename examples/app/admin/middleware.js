9cb456baf5142b80c1ef863ce7572230adcb1fa3
goliatone <burgosemi@gmail.com>
2014-11-29 23:03:34 -0500

    EXAMPLE: admin middleware reset

--- examples/app/admin/middleware.js
+++ examples/app/admin/middleware.js
@@ -3,10 +3,4 @@ var path = require('path');
 
 module.exports = function(anden){
 	console.log('/////// ADMIN MIDDLEWARE',__filename,'running');
-
-	return;
-
-	var staticPath = path.join(__dirname, 'public');
-	console.log('STATIC PATH', staticPath)
-    anden.app.use(anden.app.express.static(staticPath));
 };
